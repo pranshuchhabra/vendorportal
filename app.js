1808181d5579cb7738904de944b92d2e6654509a
pranshuchhabra <129317588+pranshuchhabra@users.noreply.github.com>
2023-04-26 18:37:30 +0530

    new

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const port = process.env.PORT || 3000;
+const path = require("path");
+const fs = require("fs");
+const app = express();
+
+
+app.set("views", path.join("views"));
+app.set("view engine", "ejs");
+
+
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+
+
+app.get('/',(req,res) => {
+    res.render("index");
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Server listening on port 3000");
+  });
+  
